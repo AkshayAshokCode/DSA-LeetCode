a73fb50276a34bb8b57068e77857b21d22cc594d
Akshay Ashok <63944420+AkshayAshokCode@users.noreply.github.com>
2024-09-07 20:56:47 +0530

    Time: 158 ms (81.21%), Space: 35.2 MB (58.39%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+        val n = digits.size
+        
+        for(i in n-1 downTo 0){
+            
+            if(digits[i] < 9){
+                digits[i]++
+                return digits
+            }
+            
+            digits[i] = 0
+        }
+        
+        val result = IntArray(n + 1)
+        result[0] = 1
+        return result
+    }
+}

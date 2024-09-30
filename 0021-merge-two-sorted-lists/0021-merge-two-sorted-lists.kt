2b9cd8306a22ada63c4cbeee93836cdc6d96bc25
Akshay Ashok <63944420+AkshayAshokCode@users.noreply.github.com>
2024-09-30 23:21:12 +0530

    Time: 188 ms (10.60%), Space: 35.3 MB (83.22%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.kt
@@ -0,0 +1,35 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+         if (list1 == null) return list2
+    if (list2 == null) return list1
+
+    val dummy = ListNode(0)
+    var current = dummy
+    var l1 = list1
+    var l2 = list2
+        
+    while (l1 != null && l2 != null) {
+        if (l1.`val` <= l2.`val`) {
+            current.next = l1
+            l1 = l1.next
+        } else {
+            current.next = l2
+            l2 = l2.next
+        }
+        current = current.next!!
+    }
+
+    current.next = l1 ?: l2
+
+    return dummy.next
+    }
+}
